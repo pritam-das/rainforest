6a12f8ba39c2a6452229d558d150211c6fd00798
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-19 22:13:58 -0400

    Added authorization to ensure users are logged in before they create reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def ensure_logged_in
+  unless current_user
+    flash[:alert] = "Please log in"
+    redirect_to new_session_url
+  end
+end
+
 def current_user
   session[:user_id] && User.find(session[:user_id])
 end
